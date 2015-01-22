8692e26163d075f7dfcf58a928f1857165028882
JarettRoman <afrocious@gmail.com>
2015-01-22 01:31:00 -0500

    revised prod script

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp.settings = {
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
